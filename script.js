149834bbe4370a1006f2624bf2997329194023f7
Ananya Kaushal <62956565+Ananya-Kaushal@users.noreply.github.com>
2024-07-13 19:46:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstWord(s) {
   // your code here
+	let s1=s.trim();
+	let substr="";
+	let space=s1.indexOf(" ");
+	if(space==-1)return s1;
+	else
+	{
+		return s1.substring(0,space);
+	}
 }
 
 // Do not change the code below
